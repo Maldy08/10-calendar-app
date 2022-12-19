6715645cba0dc0de99844eb9d0acd5ffe906a3c5
Carlos Maldonado <carlosmaldonado@MacBook-Pro-de-Carlos.local>
2022-12-18 20:35:04 -0800

    finalizando seccion 26

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,26 @@
+import { useEffect } from "react";
 import { Navigate, Route, Routes } from "react-router-dom"
 import { LoginPage } from '../auth'
 import { CalendarPage } from "../calendar";
+import { useAuthStore } from "../hooks";
 
 
 export const AppRouter = () => {
 
-  const authStatus = 'authenticated'; 
+  const { checkAuthToken, status } =  useAuthStore();
+
+   useEffect(() => {
+     checkAuthToken();
+
+   }, [])
+   
+  if( status === 'checking' ) {
+    return (
+      <h3>Cargando....</h3>
+    )
+  }
+  
+ // const authStatus = 'not-authenticated'; 
   // not-authenticated
   //authenticated
   return (
@@ -13,12 +28,20 @@ export const AppRouter = () => {
 
     <Routes>
       {
-        ( authStatus === 'not-authenticated')
-          ?  <Route path="/auth/*" element={ <LoginPage/>} />
-          :  <Route path="/*" element={ <CalendarPage/>} />
+        ( status === 'not-authenticated')
+          ?  (
+            <>
+                <Route path="/auth/*" element={ <LoginPage/>} />
+                <Route path="/*" element={ <Navigate to="/auth/login" />} />
+            </>  
+          )
+          :  (
+            <>
+                <Route path="/" element={ <CalendarPage/>} />
+                <Route path="/*" element={ <Navigate to="/" />} />
+            </>
+          )
       }
-
-      <Route path="/*" element={ <Navigate to="/auth/login" />} />
 
     </Routes>
   )
